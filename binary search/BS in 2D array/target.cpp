5528ca5bbbf8a2776c3b4bfddebbdfed7f1f38fb
Rajan <rajanmasih15@gmail.com>
2022-10-07 00:45:10 +0530

    first commit

--- binary search/BS in 2D array/target.cpp
+++ binary search/BS in 2D array/target.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+// using bruteforce technique
 int lin(int arr[3][3], int n, int target)
 {
     for(int i=0; i < n; i++)
@@ -12,30 +13,75 @@ int lin(int arr[3][3], int n, int target)
     }
     return false;
 }
-int tar(int arr[3], int n, int target)
+// using binary search 
+int search(int arr[][3], int n, int m, int row, int target)
 {
-    int s = 0;
-    int e = n - 1;
-    int mid = s + (e - s)/2;
-    while(s < e)
-    {
-        if(arr[mid] == target)
-        {
-            return true;
-        }
-        if(arr[mid] > target)
-        {
-            e = e - 1;
-        }
-        else{
-            s = s + 1;
-        }
-    }
-    return -1;
+	int s = 0;
+	int e = n - 1;
+	int mid = s + (e - s)/2;
+	while(s <= e)
+	{
+		if(arr[row][mid] == target)
+		{
+			return true;
+		}
+		if(arr[row][mid] < target)
+		{
+			s = mid + 1;
+		}
+		else
+		{
+			e = mid - 1;
+		}
+		mid = s + (e - s)/2;
+	}
+	return false;
+}
+bool binary(int arr[][3], int n, int m, int target)
+{
+	//find a row
+	int s = 0;
+	int e = n - 1;
+	int mid = s + (e - s)/2;
+	while(s <= e)
+	{
+		//check starting row
+		if(arr[mid][0] == target)
+		{
+			return true;
+		}
+		//check Ending row
+		if(arr[mid][m -1] == target)
+		{
+			return true;
+		}
+		// check if element is present in between starting and ending element
+		if(arr[mid][0] < target && target < arr[mid][m - 1])
+		{
+			//apply binary search on column
+			bool ans = search(arr, n, m, mid, target);
+			return ans;
+		}
+		//check upper part
+		if(target < arr[mid][0])
+		{
+			e = mid - 1;
+		}
+		//Check Lower part
+		else
+		{
+			s = mid + 1;
+		}
+		mid = s + (e - s)/2;
+	}
+	return false;
 }
 int main()
 {
-    int arr[3][3]={1,5,9,14,20,21,30,34,43};
+	int arr[3][3] = {1, 5, 9, 14, 20, 23, 30, 34, 43};
+	int n = 3;
+	int m = 3;
+	int target = 34;
+	cout<<binary(arr, n, m, target);
     cout<<lin(arr, 9, 20)<<endl;
-    cout<<tar(arr, 9, 20);
 }
