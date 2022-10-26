f72b482f58472bf57f5e02f1bae20e04a37cbdef
Rajan <rajanmasih15@gmail.com>
2022-10-26 06:14:26 +0530

    first commit

--- sorting/selectionsort.cpp
+++ sorting/selectionsort.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
+void SelectionS(int arr[], int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		int minIndex = i;
+		for(int j=i+1;j<n;j++)
+		{
+			if(arr[minIndex]>arr[j])
+			{
+				minIndex = j;
+			}
+		}
+		swap(arr[minIndex],arr[i]);
+	}
+}
 int main()
 {
-    return 0;
+	int arr[]= {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+	int n = 10;
+	SelectionS(arr, n);
+	for(int i=0;i<10;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
 }
