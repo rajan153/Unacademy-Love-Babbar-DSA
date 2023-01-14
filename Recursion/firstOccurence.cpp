1a8aa25ae857df2c360b6d3de0c5cc76614c1f0d
Rajan <rajanmasih15@gmail.com>
2023-01-15 00:00:07 +0530

    new

---
+++ Recursion/firstOccurence.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int firstOccurence(int arr[], int n, int index, int target)
+{
+	if(arr[index] == target)
+	{
+		return index;
+	}
+	if(index == n)
+	{
+		return -1;
+	}
+	return firstOccurence(arr, n, index+1, target);
+}
+int main()
+{
+	int arr[] = {1,3,2,3,6,4,3};
+	int n = 7;
+	int target = 3;
+	int index = 0;
+	cout<<firstOccurence(arr, n, index, target);
+	
+}
