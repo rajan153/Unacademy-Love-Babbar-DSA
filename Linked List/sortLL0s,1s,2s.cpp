702ff8ff95dcde76640e4940ff401817bbcfc1e5
Rajan <111041701+rajan153@users.noreply.github.com>
2023-02-25 23:17:03 +0530

    Create sortLL0s,1s,2s.cpp

---
+++ Linked List/sortLL0s,1s,2s.cpp
@@ -0,0 +1,60 @@
+/********************************
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+********************************/
+//Approach 1
+Node* sortList(Node *head)
+{
+    // Write your code here.
+    Node* zero = 0;
+    Node* one = 0;
+    Node* two = 0;
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        if(temp->data == 0)
+        {
+            zero++;
+        }
+        else if(temp->data == 1)
+        {
+            one++;
+        }
+        else if(temp->data == 2)
+        {
+            two++;
+        }
+        temp = temp->next;
+    }
+    temp = head;
+    while(temp != NULL)
+    {
+        if(zero != 0)
+        {
+            temp->data = 0;
+            zero--;
+        }
+        else if(one != 0)
+        {
+            temp->data = 1;
+            one--;
+        }
+        else if(two != 0)
+        {
+            temp->data = 2;
+            two--;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
